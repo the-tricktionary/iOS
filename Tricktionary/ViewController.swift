c7df79b2c23b03c45401f3dca08db19112585d7e
Marek šťovíček <marek.stovicek@pixmo.cz>
2018-11-15 00:24:16 +0100

    Initial Commit

---
+++ Tricktionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tricktionary
+//
+//  Created by Marek Šťovíček on 15/11/2018.
+//  Copyright © 2018 Marek Šťovíček. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
