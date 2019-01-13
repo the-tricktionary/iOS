9cfe7ccfcb603369465c8dab0fba9cc04cc3a8fb
Marek  Šťovíček <marek.stovicek@pixmo.cz>
2019-01-13 01:23:45 +0100

    Redirect from app to instagram and website

--- Tricktionary/Classes/App/New Group/Panel/SidePanelDelegate.swift
+++ Tricktionary/Classes/App/New Group/Panel/SidePanelDelegate.swift
@@ -19,6 +19,17 @@ class SidePanelDelegate: NSObject, UITableViewDelegate {
             viewController.delegate?.didSelectMenuItem(viewController: LoginViewController())
         } else if indexPath.row == 1 {
             viewController.delegate?.didSelectMenuItem(viewController: TricksViewController(viewModel: TricksViewModel()))
+        } else if indexPath.row == 5 {
+            let url: URL? = URL(string: Constatnts.instagramApp)
+            if UIApplication.shared.canOpenURL(url!) {
+                UIApplication.shared.open(url!)
+            } else {
+                //redirect to safari because the user doesn't have Instagram
+                UIApplication.shared.open(NSURL(string: Constatnts.instagram)! as URL)
+            }
+        } else if indexPath.row == 6 {
+            guard let url = URL(string: Constatnts.websiteUrl) else { return }
+            UIApplication.shared.open(url)
         }
         
     }
---
+++ Tricktionary/Classes/Components/Constants/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Tricktionary
+//
+//  Created by Marek  Šťovíček on 13/01/2019.
+//  Copyright © 2019 Marek Šťovíček. All rights reserved.
+//
+
+import Foundation
+
+
+struct Constatnts {
+    static let websiteUrl: String = "https://the-tricktionary.com"
+    static let instagramApp: String = "instagram://user?username=jumpropetricktionary"
+    static let instagram: String = "http://instagram.com/jumpropetricktionary"
+}
